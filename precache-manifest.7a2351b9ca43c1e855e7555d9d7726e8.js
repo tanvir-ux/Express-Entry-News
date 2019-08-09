68949217a5d50085682ec0803602997f596ec595
tanvir-ux <tanviralamshuvo@gmail.com>
2019-08-09 08:50:46 +0600

    first commit

---
+++ precache-manifest.7a2351b9ca43c1e855e7555d9d7726e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "668636a260034c5298a7",
+    "url": "/Express-Entry-News/static/js/runtime~main.668636a2.js"
+  },
+  {
+    "revision": "77d6d5827eba35208f4f",
+    "url": "/Express-Entry-News/static/js/main.77d6d582.chunk.js"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/Express-Entry-News/static/js/2.cea528bd.chunk.js"
+  },
+  {
+    "revision": "77d6d5827eba35208f4f",
+    "url": "/Express-Entry-News/static/css/main.a1e236c8.chunk.css"
+  },
+  {
+    "revision": "99dda17ca22d0d3f58c4bec37b6be8ce",
+    "url": "/Express-Entry-News/index.html"
+  }
+];
